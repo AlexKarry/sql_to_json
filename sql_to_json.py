5d333e638cba2fbbe598d4e226c689ffffb59b49
alexkarry <Karyshkov@gmail.com>
2023-09-20 16:41:11 -0400

    Add files via upload

---
+++ sql_to_json.py
@@ -0,0 +1,39 @@
+import sqlite3
+import json
+
+db_filenem = "/Users/ak6914/Desktop/tuts/Advanced_Python_NYU/python_data_apy/session_02_working_files/session_2.db"
+
+json_file = "/Users/ak6914/Desktop/tuts/Advanced_Python_NYU/python_data_apy/session_02_working_files/revenue.json"
+
+conn = sqlite3.connect(db_filenem)
+
+cursor = conn.cursor()
+
+outer_dict = {}
+
+data_from_sql = "SELECT * FROM revenue"
+cursor.execute(data_from_sql)
+
+# counter = 1
+
+for row in cursor:
+    name = row[0]
+    state = row[1]
+    cost = row[2]
+
+    inner_dict = {
+        "name": name,
+        "state": state,
+        "cost": cost
+    }
+
+    outer_dict[name] = inner_dict
+
+    # outer_dict[counter] = inner_dict
+    # counter += 1
+
+    wfh = open(json_file, 'w', newline='')
+    json.dump(outer_dict, wfh, indent=5)
+
+conn.close()
+print(outer_dict)
